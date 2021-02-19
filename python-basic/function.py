ce279bb30ff84ad886f9a26f1e6e1cc528f3c16d
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2021-02-20 08:25:43 +0900

    함수 만들기

---
+++ python-basic/function.py
@@ -0,0 +1,34 @@
+# def add(a,b):
+#   return a + b
+
+# print(add(1,2))
+
+# def add1(a,b):
+#   print("함수의 결과", a+b)
+
+# add1(3,5)
+
+# def add2(a,b):
+#   print("함수의 결과 : " , a+b)
+
+# add2(b=10, a=2)
+
+# a = 0
+
+# global로 함수 밖 변수 건들수 있음
+# def func():
+#   global a
+#   a += 1
+
+# for i in range(10):
+#   func()
+
+# print(a)
+
+# def add3(a,b):
+#   return a + b
+
+# print(add(5,6))
+
+# 람다 표현식으로 add구현 - 한줄로 가능
+# print((lambda a, b: a + b)(6,7))
